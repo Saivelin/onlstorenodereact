79e99f4a19062878b5e842f4e1adce5ffac3074e
Saivelin <vlad.pochkalov@mail.ru>
2023-03-12 10:49:17 +0300

    [id]

--- src/pages/category/[id].jsx
+++ src/pages/category/[id].jsx
@@ -1,7 +1,11 @@
+import Categories from "@/components/Categories";
+import MainBlock from "@/components/MainBlock";
+
 const ProductsToCat = () => {
     return (
-        <div>
-            ProductsToCat
+        <div className="productsToCat">
+            <Categories />
+            <MainBlock />
         </div>
     );
 };
